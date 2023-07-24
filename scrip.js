a21fa5151d7ab89049b48770c256bc7a91bf2d4f
Fernando Montes <Fernando.m.l426@gmail.com>
2023-07-24 17:24:07 -0400

    add secon part to HTML and JS variables

---
+++ scrip.js
@@ -0,0 +1,4 @@
+let hamburger = document.querySelector('.hamburger');
+let mainLinks = document.querySelector('#navItems-links');
+let linksPopUp = document.querySelectorAll('.menu-navItems');
+let closeIcon = document.querySelector('.close-button');
