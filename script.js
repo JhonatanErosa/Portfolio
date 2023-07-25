52e767c12a65ed982bd6f27509487044436f2f82
Jhonatan <jhonatancaama@gmail.com>
2023-07-24 18:06:33 -0600

    Fix linters in css and JavaScript files

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let hamburger = document.querySelector('.hamburger');
-let mainLinks = document.querySelector('#navItems-links');
-let linksPopUp = document.querySelectorAll('.menu-navItems');
-let closeIcon = document.querySelector('.close-button');
+const hamburger = document.querySelector('.hamburger');
+const mainLinks = document.querySelector('#navItems-links');
+const linksPopUp = document.querySelectorAll('.menu-navItems');
+const closeIcon = document.querySelector('.close-button');
 
-function openWindow () {
-    mainLinks.classList.replace('navItems', 'navItems-menu');
+function openWindow() {
+  mainLinks.classList.replace('navItems', 'navItems-menu');
 }
 
-function closeWindow () {
-    mainLinks.classList.replace('navItems-menu', 'navItems');
+function closeWindow() {
+  mainLinks.classList.replace('navItems-menu', 'navItems');
 }
 
 hamburger.addEventListener('click', openWindow);
 closeIcon.addEventListener('click', closeWindow);
-for (var i = 0; i < linksPopUp.length; i++){
-    linksPopUp[i].addEventListener('click', closeWindow);
+for (let i = 0; i < linksPopUp.length; i + 1) {
+  linksPopUp[i].addEventListener('click', closeWindow);
 }
