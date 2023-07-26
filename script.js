5221582433a5dfefd9ae0abc1515ce72b4dd1348
Jose Raul <johndoe@example.com>
2023-07-26 18:30:53 -0400

    Add updates in html and projects array

--- script.js
+++ script.js
@@ -16,3 +16,63 @@ closeIcon.addEventListener('click', closeWindow);
 for (let i = 0; i < linksPopUp.length; i += 1) {
   linksPopUp[i].addEventListener('click', closeWindow);
 }
+
+//PopUp Window
+let projects = [
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/MultiPostStories.svg',
+    technologies: ['CSS','HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/SnapshootPortfolio.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/Dashboard.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/WebSitePortfolio.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/SnapshootPortfolio.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/Dashboard.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+  {
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    featuredImage: 'Sources/WebSitePortfolio.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveDemo: 'https://jhonatanerosa.github.io/Portfolio/',
+    sourceURL: 'https://github.com/JhonatanErosa/Portfolio'
+  },
+];
