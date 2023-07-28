a83b7a10bfdaee6f989feb5094dff62287b7935e
Jhonatan <jhonatancaama@gmail.com>
2023-07-28 14:01:26 -0600

    Fix linters in script.js

--- script.js
+++ script.js
@@ -215,28 +215,28 @@ form.addEventListener('submit', (elem) => {
   }
 });
 
-//Local Storage
+// Local Storage
 const userName = document.querySelector('#user-name');
 const userEmail = document.querySelector('#email');
 const userMessage = document.querySelector('#msg');
 
-function saveUserData () {
+function saveUserData() {
   const userData = {
     name: userName.value,
     email: userEmail.value,
-    message: userMessage.value
+    message: userMessage.value,
   };
   localStorage.setItem('userData', JSON.stringify(userData));
-};
+}
 
-function getUSerData () {
-  if(localStorage.getItem('userData')) {
+function getUSerData() {
+  if (localStorage.getItem('userData')) {
     const userData = JSON.parse(localStorage.getItem('userData'));
-    userName.value = userData['name'];
-    userEmail.value = userData['email'];
-    userMessage.value = userData['message'];
-  };
-};
+    userName.value = userData.name;
+    userEmail.value = userData.email;
+    userMessage.value = userData.message;
+  }
+}
 
 userName.addEventListener('change', () => {
   saveUserData();
